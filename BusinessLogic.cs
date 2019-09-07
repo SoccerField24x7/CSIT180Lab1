f5f3e3cdba62c1a209d2873dfffc25f6bdb0a1b6
Jesse Quijano <jesse@quijano.net>
2019-09-06 19:06:26 -0700

    checkpoint

---
+++ BusinessLogic.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fertilizer.BL
+{
+    class BusinessLogic
+    {
+        private float length;
+        private float width;
+        private float nitro;
+
+        public BusinessLogic(float lawnLength, float lawnWidth, float nitroContent)
+        {
+            this.length = lawnLength;
+            this.width = lawnWidth;
+            this.nitro = nitroContent;
+        }
+
+        public float CalculateFertilizerPoundsRequired()
+        {
+
+            return 0;
+        }
+
+        private bool Validate()
+        {
+            if(this.length <= 0 || this.width <= 0)
+            {
+                throw new Exception("");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fertilizer.BL;
+
+namespace Fertilizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessLogic calc = new BusinessLogic(0,0,0);
+        }
+    }
+}
